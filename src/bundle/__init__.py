55886180bcbf44cc9fc21626eb7ec5fa0befca1e
Luca Salvarani <lucasalvarani99@gmail.com>
2023-01-31 13:22:27 +0100

    ✨ feat(clienti): Implementato caching per la configurazione dei clienti

--- src/bundle/__init__.py
+++ src/bundle/__init__.py
@@ -1,3 +1,5 @@
+from .path import *
+
 __ALL__ = [
 	"get_bundle_directory",
 	"is_executable"
--- src/clienti.py
+++ src/clienti.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 import pandas as pd
 import numpy as np
 
@@ -7,11 +8,17 @@ import logging
 
 import re
 
+import hashlib
+import pickle
+import bundle
+
 logger = logging.getLogger("danea-easyfatt.clienti")
 
 
 CUSTOMER_EXTRA_FIELD = 3
 REGEX_INTERVALLO = r'([0-9:]+)\s*(?:>+|-+|\s+a\s+)\s*([0-9:]+)'
+
+CACHE_FILENAME = "customer_info.pickle"
 
 def rename_extra_field(column_name):
 	"""Rinomina il campo 'Extra {N}' in 'IntervalloSpedizione'. Lascia inalterati i nomi delle altre colonne.
@@ -45,6 +52,32 @@ def formatta_orario(value):
 
 
 def get_intervallo_spedizioni(filename, extra_field_id=1):
+	md5sum = ""
+	with open(filename, "rb") as f:
+		md5sum = hashlib.md5(f.read()).hexdigest()
+	
+	logger.info(f"Hash md5 file excel: '{md5sum}'")
+
+	cache_dir = bundle.get_execution_directory() / ".cache"
+	cache_file = cache_dir / CACHE_FILENAME
+
+	# Creo il percorso se non esiste già
+	Path(cache_dir).mkdir(parents=True, exist_ok=True)
+
+	logger.debug(f"Cerco file di cache '{cache_file}'")
+	if cache_file.exists():
+		logger.debug(f"Trovato file cache '{cache_file}'") 
+		with open(cache_file, "rb") as pickle_file:
+			cached_data = pickle.load(pickle_file)
+		
+		logger.debug(f"Hash md5 pickle: '{cached_data.get('metadata', {}).get('hash', None)}'")
+		if cached_data and cached_data.get('metadata', {}).get('hash', None) == md5sum:
+			logger.info(f"Carico i dati dal file di cache.") 
+			return cached_data
+
+		logger.debug(f"File di cache invalidato. Necessaria rielaborazione dati.") 
+
+ 
 	logger.debug(f"Carico file '{filename}'")
 	df = pd.read_excel(filename, dtype=str)
 	logger.info(f"File excel caricato (trovate {df.shape[0]} righe e {df.shape[1]} colonne)")
@@ -87,11 +120,23 @@ def get_intervallo_spedizioni(filename, extra_field_id=1):
 	logger.info(f"Sanificati valori della colonna 'IntervalloSpedizione'")
 
 	logger.info(f"Informazioni cliente finali: \n{customer_info}")
-	return { 
-		customer["Cod."]: customer["IntervalloSpedizione"]
-		for customer in customer_info.to_dict('records')
-		if customer["IntervalloSpedizione"] is not None
+	return_data = {
+		"metadata": {
+			"hash": md5sum
+		},
+		"data": { 
+			customer["Cod."]: customer["IntervalloSpedizione"]
+			for customer in customer_info.to_dict('records')
+			if customer["IntervalloSpedizione"] is not None
+		}
 	}
+
+	with open(cache_file, "wb") as pickle_file:
+		pickle.dump(return_data.copy(), file=pickle_file)
+	logger.info(f"Salvato file di cache '{cache_file}'. La prossima esecuzione dovrebbe essere più veloce.")
+
+	return return_data
+
 
 if __name__ == '__main__':
 	customers = get_intervallo_spedizioni('src/ExportClienti.xlsx', extra_field_id=CUSTOMER_EXTRA_FIELD)
