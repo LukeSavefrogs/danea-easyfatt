a091926add5c98cc8aec581dcd163df419e8b70b
Luca Salvarani <lucasalvarani99@gmail.com>
2023-04-12 12:57:00 +0200

    🧪 test(updater): Aggiunto test per il modulo `updater`

---
+++ tests/test_updater.py
@@ -0,0 +1,28 @@
+import unittest
+
+import updater
+from packaging.version import Version
+
+class OperationsTestCase(unittest.TestCase):
+	def test_current_version (self):
+		self.assertRegex(
+			updater.get_current_version(),
+			r"[0-9]+\.[0-9]+\.[0-9]+.*"
+		)
+
+	def test_remote_version (self):
+		version_info = updater.get_latest_version()
+
+		self.assertEqual(len(version_info.keys()), 3)
+		
+		self.assertRegex(
+			version_info["version"],
+			r"[0-9]+\.[0-9]+\.[0-9]+.*"
+		)
+	
+	def test_version_syntax (self):
+		latest  = Version(updater.get_latest_version()["version"])
+		current = Version(updater.get_current_version())
+
+		self.assertIsInstance(latest, Version)
+		self.assertIsInstance(current, Version)
