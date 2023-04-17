daf40211abb92313da20ed622fd498a2b683c622
Luca Salvarani <lucasalvarani99@gmail.com>
2023-04-17 18:05:50 +0200

    🧪 test(configurazione): Aggiunti test per valore di default e valore vuoto

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,67 +1,22 @@
-import functools
-import os
 from pathlib import Path
 import sys
-import tempfile
-import time
 import unittest
 
-import textwrap
 from config_manager import get_configuration
 
 # Hack needed to include scripts from the `scripts` directory (under root)
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-import scripts.build as builder_script
 from tests.utils.decorators import with_temporary_file
 
 
 class ConfigurationTestCase(unittest.TestCase):
-    @with_temporary_file()
-    def test_configuration(self, temp_config_file: Path):
-        temp_config_file.write_text(textwrap.dedent("""
-            log_level = "WARNING"
-
-            [files.input]
-            easyfatt = "./EasyfattTesting.DefXml"
-            addition = ""
-
-            [features.shipping]
-            default_interval = "00:00-06:00"
-        """))
-
+    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+        [features.shipping]
+        default_interval = "00:00-06:00"
+    """)
+    def test_changed_value(self, temp_config_file: Path):
         parsed_configuration = get_configuration(temp_config_file)
-        expect_configuration = {
-            'log_level': 'WARNING', 
-            'easyfatt': {
-                'customers': {
-                    'custom_field': 1, 
-                    'export_filename': ['Soggetti.xlsx', 'Soggetti.ods']
-                }
-            }, 
-            'files': {
-                'input': {
-                    'easyfatt': './EasyfattTesting.DefXml', 
-                    'addition': ''
-                }, 
-                'output': {
-                    'csv': './Documenti.csv'
-                }
-            }, 
-            'options': {
-                'output': {
-                    'csv_template': '@{CustomerName} {CustomerCode}@{eval_IndirizzoSpedizione} {eval_CAPSpedizione} {eval_CittaSpedizione}(20){eval_intervalloSpedizione}^{eval_pesoSpedizione}^'
-                }
-            }, 
-            'features': {
-                'shipping': {
-                    'default_interval': '00:00-06:00'
-                }
-            }
-        }
-        self.assertEqual(
-            parsed_configuration,
-            expect_configuration
-        )
+        
         self.assertEqual(
             parsed_configuration,
             parsed_configuration | {
@@ -72,6 +27,49 @@ class ConfigurationTestCase(unittest.TestCase):
                 }
             }
         )
+
+    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+        [features.shipping]
+        default_interval = ""
+    """)
+    def test_empty_value(self, temp_config_file: Path):
+        parsed_configuration = get_configuration(temp_config_file)
+        
+        self.assertEqual(
+            parsed_configuration,
+            parsed_configuration | {
+                'features': {
+                    'shipping': {
+                        'default_interval': ''
+                    }
+                }
+            }
+        )
+
+    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+        [features.shipping]
+    """)
+    def test_default_value(self, temp_config_file: Path):
+        parsed_configuration = get_configuration(temp_config_file)
+        
+        self.assertEqual(
+            parsed_configuration,
+            parsed_configuration | {
+                'features': {
+                    'shipping': {
+                        'default_interval': '07:00-16:00'
+                    }
+                }
+            }
+        )
+
+    # @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+    #     [features.shipping.default_interval]
+    #     test = ""
+    # """)
+    # def test_err_value(self, temp_config_file: Path):
+    #     with self.assertRaises(Exception):
+    #         get_configuration(temp_config_file)
 
 
 if __name__ == '__main__':
