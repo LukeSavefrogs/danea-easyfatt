4efdec67a7db8fe6662ddb268f322b7ce85d2415
Luca Salvarani <lucasalvarani99@gmail.com>
2023-04-19 00:29:00 +0200

    🩺 refactor: Automatizzato recupero URL endpoint REST API
    Ora l'URL all'endpoint REST API utilizzato per controllare la versione sul server viene ricavato analizzando il campo `repository` presente nel file di configurazione di progetto `pyproject.toml` (in precedenza autore e repository erano hardcoded in due costanti)
    
    Questo permetterà in futuro di poter riutilizzare le funzioni di questo modulo in altri progetti.

--- src/updater.py
+++ src/updater.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import sys
 
 import requests
@@ -9,15 +10,44 @@ from packaging.version import Version
 
 import bundle.path as bundle
 
-REPO_OWNER = "LukeSavefrogs"
-REPO_NAME = "danea-easyfatt"
-
 logger = logging.getLogger("danea-easyfatt.updater")
 logger.addHandler(logging.NullHandler())
 
 
+def get_github_api_endpoint() -> str:
+	""" Returns the current project's Github API endpoint.
+
+	Uses the `pyproject.toml` file in the root folder of the project to determine the repository URL.
+
+	Raises:
+		Exception: If the URL is not a valid Github repository URL
+
+	Returns:
+		api_url (str): The URL of the REST API endpoint
+	"""
+	repository_url = toml.load((bundle.get_root_directory() / 'pyproject.toml').resolve()) \
+		.get("tool", {}) \
+		.get("poetry", {}) \
+		.get("repository", "")
+
+	url = re.match(r"(?:(?:https?:)?//)?(github.com)/([-A-Za-z0-9_.]+)/([-A-Za-z0-9_.]+)", repository_url)
+
+	if url is None or len(url.groups()) != 3:
+		raise Exception(f"The URL '{repository_url}' is not a valid Github repository URL.")
+	
+	(domain, author, repository) = url.groups()
+	return f"https://api.{domain}/repos/{author}/{repository}"
+
+
 def get_latest_version():
-	response = requests.get(f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/releases/latest")
+	""" Returns information about the latest release (/releases/latest).
+
+	Returns:
+		release (dict): The release information
+	"""
+	api_url = get_github_api_endpoint()
+
+	response = requests.get(f"{api_url}/releases/latest")
 	json_response = json.loads(response.text)
 
 	return { 
@@ -27,7 +57,15 @@ def get_latest_version():
 	}
 
 
-def get_current_version():
+def get_current_version() -> str:
+	""" Returns the current version as specified in the `pyproject.toml` under `tool.poetry.version`.
+
+	Raises:
+		Exception: If the `pyproject.toml` cannot be found
+
+	Returns:
+		version (str): The current version
+	"""
 	toml_file = (bundle.get_root_directory() / 'pyproject.toml').resolve()
 
 	if not toml_file.exists() or not toml_file.is_file():
@@ -38,7 +76,12 @@ def get_current_version():
 	return poetry_config["tool"]["poetry"]["version"]
 
 
-def update_available():
+def update_available() -> bool:
+	""" Checks if a new version has been released.
+
+	Returns:
+		is_available (bool): Wether a new update is available among the releases.
+	"""
 	latest  = Version(get_latest_version()["version"])
 	current = Version(get_current_version())
 	
