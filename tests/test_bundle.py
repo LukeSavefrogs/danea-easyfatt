57d3d760db2a0a0355ca7e614848bcd8dbb90b2c
Luca Salvarani <lucasalvarani99@gmail.com>
2023-07-27 18:09:17 +0200

    tests(bundle): Fix test in timeout

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -13,8 +13,10 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 from tests.utils.decorators import with_temporary_file
 import scripts.build as builder_script
 
+
 class BundleTestCase(unittest.TestCase):
-    """ Tests for the bundled executable """
+    """Tests for the bundled executable"""
+
     _executable_name: Path
     _python_scr_name: Path
 
@@ -24,9 +26,11 @@ class BundleTestCase(unittest.TestCase):
         cls._python_scr_name = Path("./src/bootstrap.py").resolve()
 
         # 1. Build the temporary executable file
-        if not builder_script.build(str(cls._python_scr_name), cls._executable_name.stem):
+        if not builder_script.build(
+            str(cls._python_scr_name), cls._executable_name.stem
+        ):
             raise Exception("Failed creating executable.")
-        
+
         # 2. Remove the temporary build spec file
         for file in Path("./").glob(cls._executable_name.stem + ".spec"):
             file.unlink()
@@ -40,61 +44,95 @@ class BundleTestCase(unittest.TestCase):
         # Remove the temporary executable file
         cls._executable_name.unlink()
 
-
-
     def test_help(self):
         command_output = subprocess.run(
-            [self._executable_name, '-h'], timeout=360,
-            input='\n', capture_output=True, check=True, text=True,
+            [self._executable_name, "-h"],
+            timeout=360,
+            input="\n",
+            capture_output=True,
+            check=True,
+            text=True,
         ).stdout
 
         self.assertRegex(command_output, r"usage:.*")
 
     def test_version(self):
         command_output = subprocess.run(
-            [self._executable_name, '--version'], timeout=360,
-            input='\n', capture_output=True, check=True, text=True,
+            [self._executable_name, "--version"],
+            timeout=360,
+            input="\n",
+            capture_output=True,
+            check=True,
+            text=True,
         ).stdout
 
         self.assertRegex(command_output, r"v[0-9]+\.[0-9]+\.[0-9]+.*")
 
-
-    def test_warning_conf_not_found (self):
-        """ Tests the behaviour of the program when the specified configuration cannot be found """
+    def test_warning_conf_not_found(self):
+        """Tests the behaviour of the program when the specified configuration cannot be found"""
         non_existent_file = f"{secrets.token_hex(32)}.toml"
 
         command_output = subprocess.run(
-            [self._executable_name, "--disable-rich-logging", "-c", non_existent_file], timeout=360,
-            input='\n', capture_output=True, text=True,
+            [self._executable_name, "--disable-rich-logging", "-c", non_existent_file],
+            timeout=360,
+            input="\n",
+            capture_output=True,
+            text=True,
         ).stdout
 
-        self.assertRegex(command_output, f"File di configurazione utente '.*?{non_existent_file}' non trovato\\.")
+        self.assertRegex(
+            command_output,
+            f"File di configurazione utente '.*?{non_existent_file}' non trovato\\.",
+        )
         self.assertRegex(command_output, "Utilizzo la configurazione di default")
 
-
-    def test_no_version_check (self):
-        """ Tests if no version check is done. """
+    def test_no_version_check(self):
+        """Tests if no version check is done."""
         command_output = subprocess.run(
-            [self._executable_name, "--disable-rich-logging", "--disable-version-check"], timeout=360,
-            input='\n', capture_output=True, text=True,
+            [
+                self._executable_name,
+                "--disable-rich-logging",
+                "--disable-version-check",
+                "--goal",
+                "csv-generator",
+            ],
+            timeout=360,
+            input="\nn\nn\n",
+            capture_output=True,
+            text=True,
         ).stdout
 
-        self.assertRegex(command_output, "Il controllo versione è stato disattivato tramite CLI")
+        self.assertRegex(
+            command_output, "Il controllo versione è stato disattivato tramite CLI"
+        )
 
-
-    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content=f"""
+    @with_temporary_file(
+        file_prefix="veryeasyfatt-",
+        file_suffix=".toml",
+        content=f"""
         [files.input]
         easyfatt = "./{secrets.token_hex(32)}.DefXml"
-    """)
-    def test_error_file_not_found (self, configuration_file: Path):
-        """ Tests the behaviour of the program when a required file is not present """
+    """,
+    )
+    def test_error_file_not_found(self, configuration_file: Path):
+        """Tests the behaviour of the program when a required file is not present"""
         command_output = subprocess.run(
-            [self._executable_name, "--disable-rich-logging", "-c", configuration_file], timeout=360,
-            input='\n', capture_output=True, text=True,
+            [
+                self._executable_name,
+                "--disable-rich-logging",
+                "-c",
+                configuration_file,
+                "--goal",
+                "csv-generator",
+            ],
+            timeout=360,
+            input="\nn\nn\n",
+            capture_output=True,
+            text=True,
         ).stdout
 
         self.assertRegex(command_output, r"File richiesto '.*\.DefXml' non trovato\.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(verbosity=2)
