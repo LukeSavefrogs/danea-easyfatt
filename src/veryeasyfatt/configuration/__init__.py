78b37d705567636654d71c0625d4834306ad9eb9
Luca Salvarani <lucasalvarani99@gmail.com>
2023-09-05 18:03:09 +0200

    refactor(config): Rimosso vecchio modulo `config_manager`

--- src/veryeasyfatt/configuration/__init__.py
+++ src/veryeasyfatt/configuration/__init__.py
@@ -4,60 +4,73 @@ from dynaconf import Dynaconf, Validator
 from veryeasyfatt import bundle
 from veryeasyfatt.configuration.schemas import SettingsSchema
 
+def _get_settings() -> Dynaconf:
+    """ FOR INTERNAL USE ONLY!
+    
+    Returns a `Dynaconf` object representing the application settings.
+    This function can be used to test the configuration.
 
-settings: SettingsSchema = Dynaconf(  # pyright: ignore[reportGeneralTypeIssues]
-    settings_files=["veryeasyfatt.config.toml", ".secrets.toml"],
-    merge_enabled=True,  # Merge all found files into one configuration.
-    validate_on_update=False,  # Validate the configuration when it is updated.
-    validators=[  # Custom validators.
-        Validator(
-            "easyfatt.database.filename",
-            default=None,
-            when=Validator("easyfatt.database.filename", eq=""),
-            cast=lambda value: None
-            if str(value).strip() == ""
-            else Path(value).expanduser().resolve(),
-        ),
-        Validator(
-            "files.input.easyfatt",
-            default=bundle.get_execution_directory() / "Documenti.DefXml",
-            when=Validator("files.input.easyfatt", eq=""),
-            cast=lambda value: Path(bundle.get_execution_directory() / "Documenti.DefXml")
-            if str(value).strip() == ""
-            else Path(value),
-        ),
-        Validator(
-            "files.input.addition",
-            default=None,
-            when=Validator("files.input.addition", eq=""),
-            cast=lambda value: None
-            if str(value).strip() == ""
-            else Path(value),
-        ),
-        Validator(
-            "files.output.csv",
-            default=bundle.get_execution_directory() / "Documenti.csv",
-            when=Validator("files.output.csv", eq=""),
-            cast=lambda value: Path(bundle.get_execution_directory() / "Documenti.csv")
-            if str(value).strip() == ""
-            else Path(value),
-        ),
-        Validator(
-            "files.output.kml",
-            default=bundle.get_execution_directory() / "output.kml",
-            when=Validator("files.output.kml", eq=""),
-            cast=lambda value: Path(bundle.get_execution_directory() / "output.kml")
-            if str(value).strip() == ""
-            else Path(value),
-        ),
-    ],
-    envvar_prefix="VERYEASYFATT",  # Prefix used by Dynaconf to load values from environment variables
-)
+    Returns:
+        Dynaconf: The application settings.
+
+    Example:
+        ```python
+        from veryeasyfatt.configuration import _get_settings
+
+        print(_get_settings() == _get_settings()) # False
+    """
+    return Dynaconf(
+        settings_files=["veryeasyfatt.config.toml", ".secrets.toml"],
+        merge_enabled=True,  # Merge all found files into one configuration.
+        validate_on_update=False,  # Validate the configuration when it is updated.
+        validators=[  # Custom validators.
+            Validator(
+                "easyfatt.database.filename",
+                default=None,
+                when=Validator("easyfatt.database.filename", eq=""),
+                cast=lambda value: None
+                if str(value).strip() == ""
+                else Path(value).expanduser().resolve(),
+            ),
+            Validator(
+                "files.input.easyfatt",
+                default=bundle.get_execution_directory() / "Documenti.DefXml",
+                when=Validator("files.input.easyfatt", eq=""),
+                cast=lambda value: Path(bundle.get_execution_directory() / "Documenti.DefXml")
+                if str(value).strip() == ""
+                else Path(value),
+            ),
+            Validator(
+                "files.input.addition",
+                default=None,
+                when=Validator("files.input.addition", eq=""),
+                cast=lambda value: None
+                if str(value).strip() == ""
+                else Path(value),
+            ),
+            Validator(
+                "files.output.csv",
+                default=bundle.get_execution_directory() / "Documenti.csv",
+                when=Validator("files.output.csv", eq=""),
+                cast=lambda value: Path(bundle.get_execution_directory() / "Documenti.csv")
+                if str(value).strip() == ""
+                else Path(value),
+            ),
+            Validator(
+                "files.output.kml",
+                default=bundle.get_execution_directory() / "output.kml",
+                when=Validator("files.output.kml", eq=""),
+                cast=lambda value: Path(bundle.get_execution_directory() / "output.kml")
+                if str(value).strip() == ""
+                else Path(value),
+            ),
+        ],
+        envvar_prefix="VERYEASYFATT",  # Prefix used by Dynaconf to load values from environment variables
+    )
+
+settings: SettingsSchema = _get_settings() # pyright: ignore[reportGeneralTypeIssues]
 
 # Setup default values for missing settings
 # if str(settings.files.output.kml).strip() == "":
 #     settings.files.output.kml = bundle.get_execution_directory() / "output.kml"
 # settings.features.kml_generation.google_api_key
-
-if __name__ == "__main__":
-    print(f"Settings: '{settings.easyfatt.database.filename}'")
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import sys
 import unittest
 
-from veryeasyfatt.app.config_manager import get_configuration
+from veryeasyfatt.configuration import _get_settings
 
 # Hack needed to include scripts from the `scripts` directory (under root)
 sys.path.append(str(Path(__file__).resolve().parent.parent))
@@ -17,15 +17,16 @@ class ConfigurationTestCase(unittest.TestCase):
         default_interval = "00:00-06:00"
     """)
     def test_changed_value(self, temp_config_file: Path):
-        parsed_configuration = get_configuration(temp_config_file)
-        
+        settings = _get_settings()
+        settings.load_file(temp_config_file)
+
         try:
-            parsed_configuration["features"]["shipping"]["default_interval"]
+            settings["features"]["shipping"]["default_interval"]
         except KeyError as e:
             self.fail(f"Parsed configuration does not have key 'features.shipping.default_interval' (error: {e})")
 
         self.assertEqual(
-            parsed_configuration["features"]["shipping"]["default_interval"],
+            settings["features"]["shipping"]["default_interval"],
             '00:00-06:00'
         )
 
@@ -34,15 +35,16 @@ class ConfigurationTestCase(unittest.TestCase):
         default_interval = ""
     """)
     def test_empty_value(self, temp_config_file: Path):
-        parsed_configuration = get_configuration(temp_config_file)
+        settings = _get_settings()
+        settings.load_file(temp_config_file)
 
         try:
-            parsed_configuration["features"]["shipping"]["default_interval"]
+            settings["features"]["shipping"]["default_interval"]
         except KeyError as e:
             self.fail(f"Parsed configuration does not have key 'features.shipping.default_interval' (error: {e})")
 
         self.assertEqual(
-            parsed_configuration["features"]["shipping"]["default_interval"],
+            settings["features"]["shipping"]["default_interval"],
             ''
         )
 
@@ -50,26 +52,18 @@ class ConfigurationTestCase(unittest.TestCase):
         [features.shipping]
     """)
     def test_default_value(self, temp_config_file: Path):
-        parsed_configuration = get_configuration(temp_config_file)
-        
+        settings = _get_settings()
+        settings.load_file(temp_config_file)
+
         try:
-            parsed_configuration["features"]["shipping"]["default_interval"]
+            settings["features"]["shipping"]["default_interval"]
         except KeyError as e:
             self.fail(f"Parsed configuration does not have key 'features.shipping.default_interval' (error: {e})")
 
         self.assertEqual(
-            parsed_configuration["features"]["shipping"]["default_interval"],
+            settings["features"]["shipping"]["default_interval"],
             '07:00-16:00'
         )
-
-    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
-        [features.shipping.default_interval]
-        test = ""
-    """)
-    def test_err_value(self, temp_config_file: Path):
-        """ The `get_configuration` function raises an Exception when a string leaf is being redefined as dict. """
-        with self.assertRaises(Exception):
-            get_configuration(temp_config_file)
 
 
 if __name__ == '__main__':
