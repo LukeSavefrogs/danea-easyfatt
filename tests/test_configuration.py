8cc94cfc1b3e5b9bec7c3a1a3c10089ed966f59d
Luca Salvarani <lucasalvarani99@gmail.com>
2023-09-09 00:51:23 +0200

    tests(config): Corretto test per valore vuoto

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -13,8 +13,10 @@ from tests.utils.decorators import with_temporary_file
 class ConfigurationTestCase(unittest.TestCase):
     maxDiff = None
 
-    def assertHasKey(self, obj, key, access_method: Literal["dict", "getattr"]="dict") -> None:
-        """ Assert that the given object has the given key. """
+    def assertHasKey(
+        self, obj, key, access_method: Literal["dict", "getattr"] = "dict"
+    ) -> None:
+        """Assert that the given object has the given key."""
         current_obj = obj
         for k in key.split("."):
             try:
@@ -25,11 +27,14 @@ class ConfigurationTestCase(unittest.TestCase):
             except KeyError:
                 self.fail(f"Object '{current_obj}' does not have key '{k}'")
 
-
-    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+    @with_temporary_file(
+        file_prefix="veryeasyfatt-",
+        file_suffix=".toml",
+        content="""
         [features.shipping]
         default_interval = "00:00-06:00"
-    """)
+    """,
+    )
     def test_changed_value(self, temp_config_file: Path):
         settings = _get_settings()
         settings.reload_settings(temp_config_file)
@@ -37,14 +42,18 @@ class ConfigurationTestCase(unittest.TestCase):
         self.assertHasKey(settings, "features.shipping.default_interval")
 
         self.assertEqual(
-            settings["features"]["shipping"]["default_interval"],
-            '00:00-06:00'
+            settings.features.shipping.default_interval,
+            "00:00-06:00",
         )
 
-    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+    @with_temporary_file(
+        file_prefix="veryeasyfatt-",
+        file_suffix=".toml",
+        content="""
         [features.shipping]
         default_interval = ""
-    """)
+    """,
+    )
     def test_empty_value(self, temp_config_file: Path):
         settings = _get_settings()
         settings.reload_settings(temp_config_file)
@@ -52,13 +61,17 @@ class ConfigurationTestCase(unittest.TestCase):
         self.assertHasKey(settings, "features.shipping.default_interval")
 
         self.assertEqual(
-            settings["features"]["shipping"]["default_interval"],
-            ''
+            settings.features.shipping.default_interval,
+            "07:00-16:00",
         )
 
-    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+    @with_temporary_file(
+        file_prefix="veryeasyfatt-",
+        file_suffix=".toml",
+        content="""
         [features.shipping]
-    """)
+    """,
+    )
     def test_default_value(self, temp_config_file: Path):
         settings = _get_settings()
         settings.reload_settings(temp_config_file)
@@ -66,15 +79,18 @@ class ConfigurationTestCase(unittest.TestCase):
         self.assertHasKey(settings, "features.shipping.default_interval")
 
         self.assertEqual(
-            settings["features"]["shipping"]["default_interval"],
-            '07:00-16:00'
+            settings.features.shipping.default_interval,
+            "07:00-16:00",
         )
+
 
 class ConfigurationValuesTestCase(unittest.TestCase):
     maxDiff = None
 
-    def assertHasKey(self, obj, key, access_method: Literal["dict", "getattr"]="dict") -> None:
-        """ Assert that the given object has the given key. """
+    def assertHasKey(
+        self, obj, key, access_method: Literal["dict", "getattr"] = "dict"
+    ) -> None:
+        """Assert that the given object has the given key."""
         current_obj = obj
         for k in key.split("."):
             try:
@@ -85,50 +101,62 @@ class ConfigurationValuesTestCase(unittest.TestCase):
             except KeyError:
                 self.fail(f"Object '{current_obj}' does not have key '{k}'")
 
-    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+    @with_temporary_file(
+        file_prefix="veryeasyfatt-",
+        file_suffix=".toml",
+        content="""
         [easyfatt.customers]
         export_filename = ["file.xlsx"]
-    """)
+    """,
+    )
     def test_single_list(self, temp_config_file: Path):
         settings = _get_settings()
         settings.reload_settings(temp_config_file)
 
         self.assertHasKey(settings, "easyfatt.customers.export_filename")
-        
+
         self.assertEqual(
             settings.easyfatt.customers.export_filename,
             [Path("file.xlsx")],
         )
 
-    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+    @with_temporary_file(
+        file_prefix="veryeasyfatt-",
+        file_suffix=".toml",
+        content="""
         [easyfatt.customers]
         export_filename = "file.xlsx"
-    """)
+    """,
+    )
     def test_single_string(self, temp_config_file: Path):
         settings = _get_settings()
         settings.reload_settings(temp_config_file)
 
         self.assertHasKey(settings, "easyfatt.customers.export_filename")
-        
+
         self.assertEqual(
             settings.easyfatt.customers.export_filename,
             [Path("file.xlsx")],
         )
 
-    @with_temporary_file(file_prefix="veryeasyfatt-", file_suffix=".toml", content="""
+    @with_temporary_file(
+        file_prefix="veryeasyfatt-",
+        file_suffix=".toml",
+        content="""
         [easyfatt.customers]
-    """)
+    """,
+    )
     def test_empty_value(self, temp_config_file: Path):
         settings = _get_settings()
         settings.reload_settings(temp_config_file)
 
         self.assertHasKey(settings, "easyfatt.customers.export_filename")
-        
+
         self.assertEqual(
             settings.easyfatt.customers.export_filename,
-            [ Path("Soggetti.xlsx"), Path("Soggetti.ods") ],
+            [Path("Soggetti.xlsx"), Path("Soggetti.ods")],
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(verbosity=2)
