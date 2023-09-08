a33855967d6b6728883dd0de5df12a6be1642d08
Luca Salvarani <lucasalvarani99@gmail.com>
2023-09-08 10:16:08 +0200

    feat(config): Cerca e carica solo file TOML

--- src/veryeasyfatt/configuration/__init__.py
+++ src/veryeasyfatt/configuration/__init__.py
@@ -22,6 +22,7 @@ def _get_settings() -> Dynaconf:
     """
     return Dynaconf(
         settings_files=["veryeasyfatt.config.toml", ".secrets.toml"],
+        core_loaders=["toml"],  # Loaders used to load the settings files.
         merge_enabled=True,  # Merge all found files into one configuration.
         validate_on_update=False,  # Validate the configuration when it is updated.
         validators=[  # Custom validators.
